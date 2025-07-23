f26e856819ab59f829b93f6bcd0ceaac2508d660
Ahmed <aa3452000@gmail.com>
2025-07-23 21:14:47 +0300

    V04

--- DemoOOP02/Program.cs
+++ DemoOOP02/Program.cs
@@ -4,20 +4,51 @@ namespace DemoOOP02
 {
     internal class Program
     {
+
+        //static int Sum(int x , int y )
+        //{
+        //    return x + y;
+        //}
+
+        //static int Sum(int x, int y , int z)
+        //{
+        //    return x + y + z;
+        //}
+
+        //static double Sum(double x, double y)
+        //{
+        //    return x + y;
+        //}
+
+        //static double Sum(double x, int y)
+        //{
+        //    return x + y;
+        //}
+
+        //static double Sum(int x, double y)
+        //{
+        //    return x + y;
+        //}
         static void Main()
         {
-            Console.WriteLine("Inheirtance");
-            //Parent parent = new Parent(1, 3);
+            //Console.WriteLine("V04");
+            //Console.WriteLine("Inheirtance");
+            ////Parent parent = new Parent(1, 3);
 
-            ////Console.WriteLine(parent);
+            //////Console.WriteLine(parent);
 
-            //parent.fun01();
-            //parent.fun02();
+            ////parent.fun01();
+            ////parent.fun02();
 
-            Child child = new Child(1, 2, 3);   
+            //Child child = new Child(1, 2, 3);   
 
-            child.fun01();
-            child.fun02();
+            //child.fun01();
+            //child.fun02();
+
+            //Sum(1, 2);
+            //Sum(1.4 , 6.2);
+            //Sum(1, 6 , 2);
+
         }
     }
 }
