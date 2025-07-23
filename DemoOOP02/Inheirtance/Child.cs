d7e6e64b78e9e80b8062fbeddbd54c69d50dd94a
Ahmed <aa3452000@gmail.com>
2025-07-23 19:53:44 +0300

    V02

---
+++ DemoOOP02/Inheirtance/Child.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP02.Inheirtance
+{
+    internal class Child : Parent 
+    {
+        public int Z { get; set; }
+
+        public Child(int X , int Y , int Z) : base(X , Y)
+        {
+            this.Z = Z; 
+        }
+
+        public override string ToString()
+        {
+            return $"X = {X}, Y = {Y} , Z = {Z}";
+        }
+
+        public void fun01()
+        {
+
+            Console.WriteLine("I'm Child[Derived] ");
+        }
+
+        public void fun02()
+        {
+
+            Console.WriteLine($"X : {X} , Y : {Y} , Z : {Z}");
+        }
+
+    }
+}
--- DemoOOP02/Program.cs
+++ DemoOOP02/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace DemoOOP02
+﻿using DemoOOP02.Inheirtance;
+
+namespace DemoOOP02
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inheirtance");
+            //Parent parent = new Parent(1, 3);
+
+            ////Console.WriteLine(parent);
+
+            //parent.fun01();
+            //parent.fun02();
+
+            Child child = new Child(1, 2, 3);   
+
+            child.fun01();
+            child.fun02();
         }
     }
 }
